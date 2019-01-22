aa1dfae46f3daa096a19ab8e7730074eb5cc8649
Pavel Proyavin <@dd.local>
2019-01-22 17:56:33 +0300

    Changelog from 220119 pt.2

--- src/js/forms.js
+++ src/js/forms.js
@@ -8,9 +8,5 @@ if(document.getElementsByClassName('phone--musk').length) {
     var bigFormPhoneMask = new IMask(
         document.getElementsByClassName('phone--musk')[1], {
         mask: '+{7}(900)000-00-00'
-    });
-    var bigFormPhoneMask = new IMask(
-        document.getElementsByClassName('phone--musk')[2], {
-        mask: '+{7}(900)000-00-00'
-    });
+});
 }
